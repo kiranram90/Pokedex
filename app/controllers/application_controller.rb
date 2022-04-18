e09f22089d14a4c37e88b85820b6c7c2993c45b6
Kiran Ram <keyrun@Kirans-MacBook-Pro.local>
2022-04-17 21:53:52 -0400

    fixed application controller issues

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,17 +3,3 @@ class ApplicationController < ActionController::Base
 helper_method :current_user, :logged_in? #gives access to methods in the views
  
 
-private
-   def current_user
-    @current_user ||= User.find_by_id(session[:user_id]) if session[:user_id]
-   end
-
-   def logged_in?
-     !!session[:user_id]
-   end
-
-   def redirect_if_not_logged_in
-      redirect_to '/' if !logged_in?
-   end
-
-end
